b702706895b9e6fa621efebc05f3288cb0195cb2
Naah Carvalho <naah_.carvalho@outlook.com>
2025-07-22 15:23:30 -0300

    chore: atualizando configuração no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal da Naah Carvalho')
+    .setDescription(
+      'Projeto Blog Pessoal da Nathalia feito em aula durante o bloco 2 da turma JS 07',
+    )
+    .setContact(
+      'Nathalia Carvalho',
+      'https://github.com/naahcarvalho',
+      'naah_.carvalho@outlook.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
