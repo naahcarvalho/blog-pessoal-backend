6c3b7720284abdc36df59a171d6e6264aa3b1574
Naah Carvalho <naah_.carvalho@outlook.com>
2025-07-22 15:11:34 -0300

    feat: atualizando controller e entity de Postagem

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,5 +1,4 @@
-import { Tema } from '../../tema/entities/tema.entity';
-import { Usuario } from '../../usuario/entities/usuario.entity';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import {
   Column,
@@ -8,28 +7,36 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Tema } from '../../tema/entities/tema.entity';
+import { Usuario } from '../../usuario/entities/usuario.entity';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Column({ length: 100, nullable: false })
   titulo: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Column({ length: 1000, nullable: false })
   texto: string;
 
+  @ApiProperty()
   @UpdateDateColumn()
   data: Date;
 
+  @ApiProperty({ type: () => Tema })
   @ManyToOne(() => Tema, (tema) => tema.postagem, {
     onDelete: 'CASCADE',
   })
   tema: Tema;
 
+  @ApiProperty({ type: () => Usuario })
   @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
     onDelete: 'CASCADE',
   })
